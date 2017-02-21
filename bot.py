d9e66683ea40448b70fb0f2084885559fd0bb050
guangshi <wenguangshi1982@gmail.com>
2017-02-22 05:15:37 +0900

    debug off

--- bot.py
+++ bot.py
@@ -1715,5 +1715,6 @@ def handle_beacon_event(event):
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
+    #app.run(debug=True, port=5000)
     # app.run(debug=True, port=5001)
