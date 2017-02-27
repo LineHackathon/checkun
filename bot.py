524e4ffae2d469817b9404118732e4f0022b632c
Hiro Ogawa <pooh_mtb@hotmail.com>
2017-02-28 07:02:56 +0900

    test

--- bot.py
+++ bot.py
@@ -1529,7 +1529,7 @@ def handle_postback_event(event):
         reply_msgs.append(TemplateSendMessage(
             alt_text=u'支払操作',
             template=ButtonsTemplate(
-                thumbnail_image_url=payment.get('receipt'),
+                # thumbnail_image_url=payment.get('receipt'),
                 title=u'支払操作',
                 text=u'この支払に対して操作しますか？',
                 actions=[
