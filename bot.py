89589f5474d04d5e72f3307e550d57de2e3735f7
Hiro Ogawa <pooh_mtb@hotmail.com>
2017-02-28 06:42:42 +0900

    bug fix

--- bot.py
+++ bot.py
@@ -1514,6 +1514,7 @@ def handle_postback_event(event):
                 ))
     elif cmd == 'modify_payment':
         eid = data['eid']
+        print eid
         payment = db.get_payment(eid)
         text = u'{}さんが{}で{}円支払いました\n'.format(get_name(payment['payment_uid']), payment['description'], get_commad_number_str(payment['amount']))
         text += u'この支払に対する支払対象者は次の通りです\n'
